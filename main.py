93737713f714d305cfb83d01b87c77057f1d0f74
danielmsousa <danjoje@gmail.com>
2022-06-14 15:12:34 -0300

    Automato aceitar cadeia vazia
    Pequena atualização para que o automato seja capaz de aceitar cadeia vazia

--- main.py
+++ main.py
@@ -37,27 +37,28 @@ while (resposta.upper() == 'S' and resposta.upper() != 'N'):
     automatos = [a]
     for automato in a.clonar('epsilon', False):
         automatos.append(automato)
+    
+    if len(cadeia) != 0:
+        for simbolo in cadeia:
+            _automatos = []
+            for automato in automatos:
+                #Processa o símbolo
+                l = [automato for automato in automato.clonar(simbolo)]
+                #permite que o automato faça transicoes epsilons
+                for aut in l:
+                    #caso haja uma epsilons entre as próximas transições ele manda um clone realizar ela
+                    if 'epsilon' in aut.estado_atual['proximos'] and aut.vivo:
+                        h = aut.clonar('epsilon')[0]
+                        #remove a anterior, pois ele deu um "pulo"
+                        pulado = h.caminho.pop(-2)
+                        h.caminho[-1] = f"{pulado}->{h.caminho[-1]}"
+                        #Adiciona o clone que foi pro epsílon na lista
+                        l.append(h)
+                for e in l:
+                    _automatos.append(e)
+                automatos = _automatos
 
-    for simbolo in cadeia:
-        _automatos = []
-        for automato in automatos:
-            #Processa o símbolo
-            l = [automato for automato in automato.clonar(simbolo)]
-            #permite que o automato faça transicoes epsilons
-            for aut in l:
-                #caso haja uma epsilons entre as próximas transições ele manda um clone realizar ela
-                if 'epsilon' in aut.estado_atual['proximos'] and aut.vivo:
-                    h = aut.clonar('epsilon')[0]
-                    #remove a anterior, pois ele deu um "pulo"
-                    pulado = h.caminho.pop(-2)
-                    h.caminho[-1] = f"{pulado}->{h.caminho[-1]}"
-                    #Adiciona o clone que foi pro epsílon na lista
-                    l.append(h)
-            for e in l:
-                _automatos.append(e)
-            automatos = _automatos
-
-    for pos, automato in enumerate(_automatos):
+    for pos, automato in enumerate(automatos):
         print(f"[automato-{pos + 1}]:{automato.caminho} |-> {is_automato_aceito(automato)}")
 
     is_cadeia_aceita(automatos, aceito)
